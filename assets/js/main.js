1cb1c1418e46e067617f0dc103bc6d258a6b38b3
Melissa Reyna <melissahaydee21@gmail.com>
2024-03-14 00:32:52 -0400

    Implemented scroll sections active-link

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,26 @@ const scrollHeader = () => {
                        : header.classList.remove('bg-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+
+/* =============== SCROLL SECTIONS ACTIVE LINK =============== */
+/* Implemented functionality to highlight active navigation links 
+based on scroll position */
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollY = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
